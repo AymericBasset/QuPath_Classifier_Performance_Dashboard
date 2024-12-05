92f30ee876419bf601cf693054fa7debcb0e496b
BASSET Aymeric <aymeric.basset@icm-institute.org>
2024-12-05 14:02:06 +0100

    ffs

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import sklearn
+# import sklearn
 import pandas as pd
 import numpy as np
-from sklearn.metrics import roc_curve, auc, precision_recall_curve, classification_report, confusion_matrix
+# from sklearn.metrics import roc_curve, auc, precision_recall_curve, classification_report, confusion_matrix
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
 from scipy.stats import chi2_contingency
