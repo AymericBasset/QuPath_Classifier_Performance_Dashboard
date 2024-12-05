e8e75ab830dd3d5823fd3f33358ea2939f4522d4
BASSET Aymeric <aymeric.basset@icm-institute.org>
2024-12-05 14:22:39 +0100

    ffs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sklearn
 import streamlit as st
 
-st.set_page_config(layout='wide')
-st.title("Classifier Performance Evaluation")
+# st.set_page_config(layout='wide')
+# st.title("Classifier Performance Evaluation")
